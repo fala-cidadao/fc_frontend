ba9d148a6dc4f1f02c9956ca517d6992f3a308bd
Gabriel <gabriel@upplify.com>
2020-10-24 20:08:10 -0300

    add signup page

---
+++ src/pages/Signup/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+import bg from '../../assets/Web/PNG/bg.png'
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bg});
+  background-size: cover;
+
+  & img {
+    width: 50%;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & .control {
+    margin: 5px;
+
+    span {
+      pointer-events: all !important;
+      cursor: pointer;
+    }
+  }
+
+  & input {
+    padding: 10px;
+    background: ${(props) => props.theme.colors.contrast};
+    border: none;
+    width: 350px;
+    border-radius: 15px;
+    font-size: 14px;
+  }
+
+  & input:focus {
+    border: none;
+    outline: none;
+  }
+
+  & input::placeholder {
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: 14px;
+  }
+
+  & button {
+    background: ${(props) => props.theme.colors.success};
+    width: 100%;
+    border-radius: 15px;
+    color: ${(props) => props.theme.colors.background};
+    margin-top: 20px;
+    font-size: 25px;
+    padding: 5px;
+  }
+`
+
+export const OptionBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 300px;
+  justify-content: center;
+
+  & a {
+    color: ${(props) => props.theme.colors.secondary};
+    font-size: 14px;
+    margin-top: 15px;
+  }
+`
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+const Signup = React.lazy(() => import('./pages/Signup'));
 const Login = React.lazy(() => import('./pages/Login'));
 
 /* const PrivateRoute = ({ component: Component, ...rest }) => (
@@ -26,6 +27,7 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path='/login' component={Login}></Route>
+        <Route path='/signup' component={Signup}></Route>
       </Switch>
     </BrowserRouter>
   );
