0acd9b9d89d2b5d47a3f7258dda375224d6c0bc9
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2020-11-14 12:55:34 -0300

    feat: finalização da área de problemas mockada

--- src/pages/Dashboard/Problems/index.tsx
+++ src/pages/Dashboard/Problems/index.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Problem } from '../../../@types/problems';
 import ProblemItem from '../../../components/ProblemItem';
-import { Container } from './styles';
+import { Container, Filter, List, RowBox, Select, FilterRadio } from './styles';
 
+import { Icon } from '@iconify/react';
+import bxSearch from '@iconify/icons-bx/bx-search';
+import bxBell from '@iconify/icons-bx/bx-bell';
 import filter from '../../../utils/filter';
 
 const Problems: React.FC = () => {
+  const [selected, setSelected] = useState<string>('date');
   const [data, setData] = useState<Problem[]>([
     {
       title: 'teste',
@@ -25,9 +29,58 @@ const Problems: React.FC = () => {
 
   return (
     <Container>
-      {data.map((elem) => {
-        return <ProblemItem problem={elem} />;
-      })}
+      <Filter>
+        <RowBox>
+          <div className='field'>
+            <p className='control has-icons-right'>
+              <input
+                className='input'
+                type='email'
+                placeholder='Busque alguma reclamação'
+              />
+              <span className='icon is-small is-right'>
+                <Icon icon={bxSearch} style={{ fontSize: '20px' }} />
+              </span>
+            </p>
+          </div>
+          <Icon
+            className='bell'
+            icon={bxBell}
+            style={{ fontSize: '30px', cursor: 'pointer' }}
+          />
+        </RowBox>
+
+        <RowBox>
+          <div>
+            <span>Filtrar por: </span>
+            <Select
+              onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                setSelected(e.target.value)
+              }
+              value={selected}
+            >
+              <option value='date'>Data</option>
+              <option value='sector'>Setor</option>
+              <option value='title'>Título</option>
+            </Select>
+          </div>
+          <RowBox>
+            <FilterRadio>
+              <input name='filterRadio' type='radio' />
+              <label>Concluído</label>
+            </FilterRadio>
+            <FilterRadio>
+              <input name='filterRadio' type='radio' />
+              <label>Em andamento</label>
+            </FilterRadio>
+          </RowBox>
+        </RowBox>
+      </Filter>
+      <List>
+        {data.map((elem) => {
+          return <ProblemItem key={elem.title} problem={elem} />;
+        })}
+      </List>
     </Container>
   );
 };
--- src/pages/Dashboard/Problems/styles.ts
+++ src/pages/Dashboard/Problems/styles.ts
@@ -1,10 +1,52 @@
 import styled from 'styled-components';
 
-export const Container = styled.ul`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
   padding: 10px 70px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  .bell {
+    background-color: ${(props) => props.theme.colors.contrast};
+    color: ${(props) => props.theme.colors.success};
+    width: 45px;
+    height: 45px;
+    padding: 5px;
+    border-radius: 7px;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
   border-radius: 7px;
   width: 100%;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const FilterRadio = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  margin-left: 15px;
+  color: black;
+`;
+
+export const RowBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px 0 0;
+`;
+
+export const Select = styled.select`
+  border: none;
+  background: none;
+  font-size: 16px;
 `;
