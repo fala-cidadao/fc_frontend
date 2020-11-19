263792f69e934dd2c506799fc64125845aed2b46
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2020-11-18 22:28:29 -0300

    fix routes imports

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-const Login = React.lazy(() => import('./pages/Login'));
+const Login = React.lazy(() => import('../pages/Login'));
 const RequestRecoverPassword = React.lazy(
-  () => import('./pages/Request-Recover-Password')
+  () => import('../pages/Request-Recover-Password')
 );
-const Home = React.lazy(() => import('./pages/Home'));
-const RecoverPassword = React.lazy(() => import('./pages/Recover-Password'));
+const Home = React.lazy(() => import('../pages/Home'));
+const RecoverPassword = React.lazy(() => import('../pages/Recover-Password'));
 const Dashboard = React.lazy(() => import('../pages/Dashboard'));
 
 /* const PrivateRoute = ({ component: Component, ...rest }) => (
