168bf52e0721a7f91a1e1083ed3a6beca245ad8a
João Felipe <joao.freitas@ccc.ufcg.edu.br>
2020-11-24 23:36:22 -0300

    Revert "Revert "Feature/map""

--- src/routes/dashboard.routes.tsx
+++ src/routes/dashboard.routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SingleProblem from '../pages/Dashboard/SingleProblem';
 const Problems = React.lazy(() => import('../pages/Dashboard/Problems'));
+const Map = React.lazy(() => import('../pages/Dashboard/Map'));
 
 export default function DashboardRoutes() {
   return (
     <Switch>
       <Route path='/dashboard/problems' component={Problems} />
       <Route path='/dashboard/problem/:id' component={SingleProblem} />
+      <Route path='/dashboard/map' component={Map} />
     </Switch>
   );
 }
